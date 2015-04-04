77e6949b88acf01d7047422e48c8df6c8a84e7a5
kingslou <loujin_007@126.com>
2015-04-04 14:31:47 +0800

    edit

--- src/com/example/myasyntask/MainActivity.java
+++ src/com/example/myasyntask/MainActivity.java
@@ -31,9 +31,7 @@ public class MainActivity extends ActionBarActivity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
+		
 		return super.onOptionsItemSelected(item);
 	}
 }
